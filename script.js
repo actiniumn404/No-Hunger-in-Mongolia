78405d8c1bd2f961eb9ad51c940b678e55aaa5d4
actiniumn404 <83673744+actiniumn404@users.noreply.github.com>
2022-05-29 12:48:01 -0700

    Stat #2

--- script.js
+++ script.js
@@ -12,40 +12,67 @@ const fadein = (selector) => {
 	return fadeInTimeline;
 };
 
-const malnorish_cont = document
-	.getElementById("stats__malnourishment")
-	.getContext("2d");
-malnorish = new Chart(malnorish_cont, {
-	type: "doughnut",
-	data: {
-		labels: ["Malnourished people in Mongolia", "Nourished people in Mongolia"],
-		datasets: [
-			{
-				data: [35, 65],
-				backgroundColor: ["red", "green"],
-			},
-		],
-	},
-	height: 100,
-	options: {
-		responsive: true,
-		plugins: {
-			legend: {
-				position: "top",
-			},
-			title: {
-				display: true,
-				text: "Chart.js Doughnut Chart",
+const makeCanvas = (container, label, dataset, colorset) => {
+	cont = container.getContext("2d");
+	res = new Chart(cont, {
+		type: "doughnut",
+		data: {
+			labels: label,
+			datasets: [
+				{
+					data: dataset,
+					backgroundColor: colorset,
+				},
+			],
+		},
+		height: 100,
+		options: {
+			responsive: true,
+			plugins: {
+				legend: {
+					position: "top",
+				},
+				title: {
+					display: true,
+					text: "Chart.js Doughnut Chart",
+				},
 			},
 		},
-	},
-});
+	});
+	return res;
+};
+
+makeCanvas(
+	document.getElementById("stats__malnourishment"),
+	["Malnourished people in Mongolia", "Nourished people in Mongolia"],
+	[35, 65],
+	["red", "green"]
+);
+
+makeCanvas(
+	document.getElementById("stats__ulaanbaatar_food"),
+	[
+		"People with proper access to food in Ulaanbaatar",
+		"People without proper access to food in Ulaanbaatar",
+	],
+	[68, 32],
+	["yellow", "lightgreen"]
+);
 
 new ScrollMagic.Scene({
 	triggerElement: "#stats",
-	//triggerHook: "onEnter",
-	offset: 10,
+	triggerHook: 0.7,
 })
 	.setTween(fadein(document.querySelectorAll("#stats > *")[0]))
-	.duration(1000)
+	.duration(700)
 	.addTo(controller);
+
+for (let i = 1; i < document.querySelectorAll("#stats > *").length; i++) {
+	new ScrollMagic.Scene({
+		triggerElement: document.querySelectorAll("#stats > *")[i - 1],
+		triggerHook: "onEnd",
+	})
+		.setTween(fadein(document.querySelectorAll("#stats > *")[i]))
+		.duration(700)
+		.addTo(controller);
+}
