85456682ff412a114885c91fef64202a02e5903f
actiniumn404 <83673744+actiniumn404@users.noreply.github.com>
2022-06-01 19:51:31 -0700

    General Info

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ const makeBarCanvas = (container, thelabels, dataset, colorset) => {
 			labels: thelabels,
 			datasets: [
 				{
-					label: "Lack of Vitamin D",
+					label: "Extent",
 					data: dataset,
 					backgroundColor: colorset,
 				},
@@ -113,6 +113,17 @@ makeBarCanvas(
 	["orange", "gold"]
 );
 
+makeBarCanvas(
+	document.getElementById("genInfo_howbad_chart"),
+	[
+		"Food source ran very low",
+		"Worried about not having enough food",
+		"Had to eat less food",
+	],
+	[72, 47, 34],
+	["red", "yellow", "lightgreen"]
+);
+
 new ScrollMagic.Scene({
 	triggerElement: "#stats",
 	triggerHook: 0.7,
@@ -124,9 +135,28 @@ new ScrollMagic.Scene({
 for (let i = 1; i < document.querySelectorAll("#stats > *").length; i++) {
 	new ScrollMagic.Scene({
 		triggerElement: document.querySelectorAll("#stats > *")[i - 1],
-		triggerHook: "onCenter",
+		triggerHook: "onEnd",
 	})
 		.setTween(fadein(document.querySelectorAll("#stats > *")[i]))
-		.duration(700)
+		.duration(1000)
 		.addTo(controller);
 }
+
+answers = {
+	defprob:
+		"In Mongolia, a poor country to the north of China, many people are going hungry. We need to stop this.",
+	chooseWhat:
+		"I have chosen the second global goal of No Hunger. I choose this goal for the following reason: You can be poor and still be alive, but you cannot be alive without food. I have heard of how poor and hungry Mongolians are, and that is really saddening, as you know that without help, these hungry Mongolians will likely die. ",
+	howBad:
+		"Most of the people affected are impoverished people usually in the countryside, who usually are traditional Mongolian herders. The thing is, 28.4% of Mongolia lives below the poverty line, which is a lot. 22% of Mongolia do not have regular food sources to live a healthy life. Also, during COVID:",
+	dontDoAnything:
+		"If this hunger problem is not solved, then many people will stave, which usually leads to death. Moreover, many Mongolians are forced to take out loans which they cannot pay back to feed themselves. So, if they don’t die, Mongolians will end up in a financial crisis. ",
+	scienceFactors:
+		"Yes! The main reason that this hunger is happening is because of something called a dzud. A dzud is a harsh winter after a dry summer. During dzuds, the pastures that livestock graze on are frozen solid under deep snow. Livestock, which hearding families rely on for food, transportation, and income do not survive dzuds. Due to climate change, dzuds have become very common. ",
+	personalStory:
+		"Shaariibuu Luttumur is a Mongolian herder. Like many other headers, he is struggling with debt. In dzuds, herders find it very difficult to feed their livestock, so they are forced to take out loans to buy food. These loans have sent Shaariibuu and many other herders into a financial crisis. He recalls that he would have “taken out another loan to get [himself] all through the winter” Luckily, the Food and Agriculture Organization of the United Nations (FAO) intervened, and sent him some money and 846 kg of concentrated feed. Shaariibuu is now out of debt and can care for his animals now.",
+};
+
+document.querySelectorAll(".fill").forEach((e) => {
+	e.innerHTML = answers[e.getAttribute("name")];
+});
