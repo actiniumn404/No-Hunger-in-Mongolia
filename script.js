abbc4f8d8a4b7d98e210f6c9fc5212fe1d0526e1
actiniumn404 <83673744+actiniumn404@users.noreply.github.com>
2022-06-08 16:59:06 -0700

    Bullet point everything

--- script.js
+++ script.js
@@ -148,52 +148,89 @@ for (let i = 1; i < document.querySelectorAll("#stats > *").length; i++) {
 		.addTo(controller);
 }
 
+
+const makeList = (...elements) => {
+	res = "<ul>";
+	elements.forEach((el) => {
+		res += `<li>${el}</li>`;
+	});
+	res += "</ul>";
+	return res;
+};
+
 answers = {
 	defprob:
 		"In Mongolia, a poor country to the north of China, many people are going hungry. We need to stop this.",
-	chooseWhat:
-		"I have chosen the second global goal of No Hunger. I choose this goal because you can be poor and alive, but you cannot be deprived of food and stay alive. I have heard of how poor and hungry Mongolians are, which is very saddening as you know that without help, these hungry Mongolians will likely die. ",
-	howBad:
-		"Most of the people affected are impoverished Mongolian herders living in the countryside. The thing is, 28.4% of Mongolia lives below the poverty line, which is a lot. 22% of Mongolia does not have regular food sources to live a healthy life. The following graph shows the food supplies of families during COVID:",
-	dontDoAnything:
-		"If this hunger problem is not solved, then many people will starve, which usually leads to death. In addition, many Mongolians are forced to take out loans that they cannot pay back to feed themselves. So if they don’t die, Mongolians will wind up in a financial crisis. ",
-	scienceFactors:
-		'Yes! A weather phenominom known as a "dzud" is the main reason there is hunger in Mongolia. A dzud is a harsh winter after a dry summer. This causes the pastures that livestock grazes on are frozen solid under deep snow. Livestock (which herding families rely on for food, transportation, and income) do not survive dzuds. Due to climate change, dzuds have become very common. ',
+	chooseWhat: makeList(
+		"I have chosen the second global goal of No Hunger. This goal aims for having no people hungry.",
+		"Motivation: If you aren't fed and are hunger, you will likely die.",
+		"Many Mongolians are really hungry.",
+		"Without help, they will die",
+		"This is saddening, so I choose this goal"
+	),
+	howBad: makeList(
+		"Most of the people affected are impoverished Mongolian herders living in the countryside.",
+		"28.4% of Mongolia lives below the poverty line",
+		"22% of Mongolia does not have regular food sources to live a healthy life.",
+		"(Shown in graph below) During COVID-19, 72% of food sources in families ran low (red column)"
+	),
+	dontDoAnything: makeList(
+		"As stated above, if we don't do anything, Mongolians will die!",
+		"Some Mongolians take out loans to buy food to feed themselves",
+		"So, if they don't die, they will end up in a financial crisis!!! (This is bad)"
+	),
+	scienceFactors: makeList(
+		'A weather pattern known as a "dzud" is the main reason there is hunger in Mongolia',
+		"Basically, a harsh winter after a dry summer. ",
+		"During dzuds, the pastures livestock graze on are inaccessible and useless.",
+		"Then Livestock (which herding families rely on for food) die. Hence, hunger in Mongolia.",
+		"Due to climate change, dzuds have become very common. "
+	),
 	personalStory:
 		"Clip of a video made by the Food and Agriculture Organization of the United Nations (FAO) about their Mongolian Early Warning Program:",
-	sol1ex:
-		"Dzuds, the main cause of hunger in Mongolia have been increasingly common over the years due to climate change. Thus, dzuds can become less common if climate change is minimized ",
+	sol1ex: makeList(
+		"Dzuds are becoming more common due to climate change",
+		"A lot of Mongolia is at risk of dzuds (shown in map below)",
+		"You can lower dzud count by minimizing climate change "
+	),
 	//sol2ex:
 	//	"Unfortunately, not everyone knows about the global goals. Surveys found that only between 28-45% of people have even heard of the global goals. Imagine if the rest of the 55-72% of the world population knew about the global goals. Many more people would be aware, and hence, more people can make a difference. ",
-	sol3ex:
-		"Donating money is one of the simplest things you can do, but it is one of the most powerful things that can be done. According to the FAO, an organization working in Mongolia, every 1 US Dollar they spent resulted in 7.1 US Dollars in benefits in households ",
-	sol4ex:
-		'One way to survive a dzud is to move to pastures that are not affected by dzuds. This migration is called "otor." Access to these pastures isn’t the best, so paving a road to distant pastures could help ',
-	sol5ex:
-		"Livestock starve to death due to a lack of grass when the ground is frozen solid during dzuds. Mongolians depend on these livestock for many things which include food. Hence, by feeding these livestock, you can save them. ",
+	//sol3ex:
+	//"Donating money is one of the simplest things you can do, but it is one of the most powerful things that can be done. According to the FAO, an organization working in Mongolia, every 1 US Dollar they spent resulted in 7.1 US Dollars in benefits in households ",
+	sol4ex: makeList(
+		"Ancient (but kind of dangerous) Mongolian tactic to survive dzuds: otor",
+		"Isolate yourself and your herd in distant pastures that are unaffected by dzuds",
+		"Access to distant pastures isn't that great",
+		"If you pave more roads to distant pastures, more people will go on otor."
+	),
+	//sol5ex:
+	//"Livestock starve to death due to a lack of grass when the ground is frozen solid during dzuds. Mongolians depend on these livestock for many things which include food. Hence, by feeding these livestock, you can save them. ",
 	sol1pro:
-		"Despite how much money or awareness you raise, dzuds will still be a thing due to climate change. You can uproot this problem with this solution.",
+		"With this, dzuds will only rarely happen, and not kill as many livestock.",
 	sol1con: "Climate change isn’t exactly the most controllable of things ",
 	//sol2pro:
 	//"This is easy to do. Just phone your friends and family. That doesn’t really take that much effort. ",
 	//sol2con:
 	//"More awareness doesn’t necessarily result in more difference. So, this could become effort for nothing ",
-	sol3pro:
-		"Unlike stopping climate change, donating money can be done very simply. ",
-	sol3con:
-		"donating money is technically losing hard-earned money, so doing this could make money earned after working hard disappear.  ",
+	//sol3pro:
+	//"Unlike stopping climate change, donating money can be done very simply. ",
+	//sol3con:
+	//	"donating money is technically losing hard-earned money, so doing this could make money earned after working hard disappear.  ",
 	sol4pro:
 		"Roads are permanent, so you can save herders for many years with a road. ",
 	sol4con: "Roads take a lot of resources, and could take very long to make.",
-	sol5pro:
-		"You can kill two birds with one stone here: the hunger of both the livestock and the Mongolian herders ",
-	sol5con: "Food is expensive.  ",
+	//sol5pro:
+	//"You can kill two birds with one stone here: the hunger of both the livestock and the Mongolian herders ",
+	//sol5con: "Food is expensive.  ",
 	org1: "The FAO has a program called “acting early” where they give out money and food to poorer Mongolians. This program has helped feed and save many Mongolians",
 	org2: "This organization has worked in Mongolia since 2001 and has helped communities invest in protection against harsh living conditions and harsh weather, which is one of the main causes of hunger.",
 	//whybestsol:
 	//	"This is the most efficient and effective solution since a road is long term, unlike donating money or sending in food. You don’t need to return each year to redo help. This is a lot more efficient than the climate change solution as climate change isn’t the most controllable thing. ",
-	economicimpact:
-		"The thing is, Agriculture makes up 35% of the labor force, and 12% of Mongolia’s GDP. 95% of agricultural land is pastures for livestock. 70% of these have been severely degraded. We know that the livestock industry is having a bit of a problem lately which would technically kind of downgrade the 12% of the GDP agriculture dominates. Having this part lost would have a nice impact on the GDP. ",
+	economicimpact: makeList(
+		"Agriculture and Livestock makes up 35% of the labor force, and 12% of Mongolia’s GDP. (see graph below)",
+		"95% of agricultural land is pastures for livestock, 70% of which are severely degraded.",
+		"If we prevent less livestock from dying with this solution, this 12% could technically go up "
+	),
 	bib: `
 	"Acting Early to Save Livelihoods in Mongolia.” Food and Agriculture Organization of the United Nations, United Nations, 1 Sept. 2019, https://www.fao.org/fao-stories/article/it/c/1174186/.  
 
@@ -221,14 +258,25 @@ answers = {
 	),
 	suppliesneeded:
 		"This solution boils down to just building any type of road along otor migration routes. Hence, you just need basic road building materials, such as asphalt or even just gravel.",
-	viable:
-		"Mongolia has recently begun to tap into its natural resources, which include gold, copper, and coal. This has made Mongolia a bit richer. Hence, this solution can be achievable because of this extra money. Furthermore, as mentioned above, roads last a long time. You don’t need to repave roads yearly. Hence, this solution is quite efficient. ",
-	howmuchmoney:
-		"Experts estimate that $3.4-3.5 billion a year will be needed in developing countries like Mongolia to meet global goals like Zero Hunger. In response to a dzud that killed millions of livestock, the government launched the National Programme on Livestock Protection from Drought and Dzud Disasters. The money mentioned would probably go here. ",
+	viable: makeList(
+		"Mongolia is rich in natural resources. (See map below)",
+		"Government has been tapping into these. So you could fund this solution with this extra money",
+		"BUT, we could also get some money with donations",
+		"This is efficient because roads last a long time:",
+		"You don't have to repave a road every year"
+	),
+	howmuchmoney: makeList(
+		"Experts estimate that $3.4-3.5 billion a year will be needed in Mongolia to meet Zero Hunger.",
+		"Mongolia's government recently launched the <span class='timesfont'>National Programme on Livestock Protection from Drought and Dzud Disasters.</span>",
+		"This money would probably go here. "
+	),
 	effects:
 		"The short and long-term effects of this solution are pretty much equivalent. Roads that improve access for otor would cause more people to go on otor, which may save their life and livestock (which are their food source). <b>What I’m trying to say is, more people and animals are going to be saved, both in the short and long run.</b> ",
-	science:
-		"The science and logic behind this solution are really simple. When one place has a dzud, another place will likely not because of how weather works. Mongolian herders use a tactic called otor to migrate to far away pastures. You can help otor by building roads. ",
+	science: makeList(
+		"When one place has a dzud, another place will likely not because of how weather works.",
+		"Mongolian herders take advantage of this and go to distant pastures in hope of surviving a dzud. ",
+		"You can encourage this by building roads"
+	),
 	timeline:
 		"There isn’t a straightforward answer for this, but the goal is 2030. The timing of this solution relies solely on how fast Mongolia can obtain road materials and build roads. I cannot reliably calculate how long this solution will take even with data, so I will just leave it like this. ",
 	obstacle1:
